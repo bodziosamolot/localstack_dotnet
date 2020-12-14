adaf00e64c62d410d52543f27d653e1308ebe33c
JaroslawOgieglo <JaroslawOgieglo@DESKTOP-8APBS09>
2020-12-14 08:39:58 +0100

    in progress

--- Services/SqsListener.cs
+++ Services/SqsListener.cs
@@ -10,33 +10,41 @@ using Microsoft.Extensions.Logging;
 
 namespace localstack_dotnet.Services
 {
-    public class SqsListener: BackgroundService
+    public class SqsListener : IHostedService
     {
         private readonly ILogger<SqsListener> _logger;
         private readonly IConfiguration _configuration;
-        
+
         public SqsListener(ILogger<SqsListener> logger, IConfiguration configuration)
         {
             _logger = logger;
             _configuration = configuration;
         }
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        public async Task StartAsync(CancellationToken stoppingToken)
         {
+            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+
+            //Reading configuration
+            var aswSection = _configuration.GetSection("Aws");
+            var accessKey = aswSection.GetSection("AccessKey").Value;
+            var secretKey = aswSection.GetSection("SecretKey").Value;
+            var sqsUrl = aswSection.GetSection("SQSUrl").Value;
+            var localstackUrl = aswSection.GetSection("localstackUrl").Value;
+
+            _logger.LogInformation("SqsUrl: {sqsUrl}", sqsUrl);
+            _logger.LogInformation("LocalstackUrl: {localstackUrl}", localstackUrl);
+
+            //Creating sqs client
+            var credentials = new Amazon.Runtime.BasicAWSCredentials(accessKey, secretKey);
+            var awsConfig = new AmazonSQSConfig
+            {
+                ServiceURL = localstackUrl
+            };
+            AmazonSQSClient amazonSQSClient = new AmazonSQSClient(awsConfig);
+
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-
-                //Reading configuration
-                var aswSection = _configuration.GetSection("Aws");
-                var accessKey = aswSection.GetSection("AccessKey").Value;
-                var secretKey = aswSection.GetSection("SecretKey").Value;
-                var sqsUrl = aswSection.GetSection("SQSUrl").Value;
-
-                //Creating sqs client
-                var credentials = new Amazon.Runtime.BasicAWSCredentials(accessKey, secretKey);
-                AmazonSQSClient amazonSQSClient = new AmazonSQSClient(credentials, Amazon.RegionEndpoint.USEast2);
-
                 //Receive request
                 ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(sqsUrl);
                 var response = await amazonSQSClient.ReceiveMessageAsync(receiveMessageRequest, stoppingToken);
@@ -45,19 +53,26 @@ namespace localstack_dotnet.Services
                 {
                     foreach (Message message in response.Messages)
                     {
-                        Console.WriteLine($"Message received");
-                        Console.WriteLine($"Message: {message.Body}");
+                        Console.WriteLine($"Dotnet listener received a message");
+                        Console.WriteLine($"Content: {message.Body}");
 
                         //Deleting message
                         var deleteMessageRequest = new DeleteMessageRequest(sqsUrl, message.ReceiptHandle);
                         await amazonSQSClient.DeleteMessageAsync(deleteMessageRequest, stoppingToken);
 
-                        Console.WriteLine($"Message deleted");
+                        Console.WriteLine($"Dotnet listener removed the consumed message from queue");
                     }
                 }
 
                 await Task.Delay(5000, stoppingToken);
             }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine($"Sopping listener");
+
+            return Task.CompletedTask;
         }
     }
 }
