333ca9e41a1c66b470466ea90c4bee8b15ebc48e
JaroslawOgieglo <JaroslawOgieglo@DESKTOP-8APBS09>
2020-12-15 08:45:15 +0100

    in progress

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -11,29 +11,14 @@ namespace localstack_dotnet.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing2s", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
-            _logger = logger;
         }
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return null;
         }
     }
 }
